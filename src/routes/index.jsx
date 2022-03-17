5901b80a5b16d166eba4ba79070f5649cc8a141d
emhaarifin <101muhammadarifin@gmail.com>
2022-03-18 06:33:09 +0700

    feat: finish routing web app

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,29 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const ErrorBoundary = lazy(() => import('pages/ErrorBoundary'));
-
+const Login = lazy(() => import('pages/Login'));
+const PublicRoute = lazy(() => import('./publicRoute'));
+const UserList = lazy(() => import('pages/UsersList'));
+const UserDetail = lazy(() => import('pages/UserDetail'));
+const Default = lazy(() => import('layouts/default'));
+const NotFound = lazy(() => import('pages/NotFound'));
 const RouterApp = () => {
   return (
     <Suspense fallback={<></>}>
       <ErrorBoundary>
         <BrowserRouter>
-          <Routes></Routes>
+          <Routes>
+            <Route path='/' element={<Default />}>
+              <Route element={<PublicRoute />}>
+                <Route index element={<Login />} />
+              </Route>
+              <Route path='users'>
+                <Route index element={<UserList />} />
+                <Route path=':id' element={<UserDetail />} />
+              </Route>
+            </Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
         </BrowserRouter>
       </ErrorBoundary>
     </Suspense>
